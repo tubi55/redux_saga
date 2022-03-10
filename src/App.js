a5b26b52ec81000d4bb0d092c653fb49b22a76f6
tubi55 <tubi55@nate.com>
2022-03-10 21:17:13 +0900

    redux-saga 주석 해설 버전

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import *  as actions from './redux/action';
 
 export default function App() {
+  //검색어를 담을 state생성
   const [tag, setTag] = useState('ocean');
+  //구조분해 할당으로 store.flickrReducer안의 객체에서 flickr값만 뽑음
   const {flickr} = useSelector( store => store.flickrReducer );
   const dispatch = useDispatch();
   console.log(flickr);
 
+  //컴포넌트 생성시 FLICKR_START액션타입에 tag를 담아서 reducer에 전달
   useEffect(()=>{
     dispatch({type: actions.FLICKR_START, tag })
   },[]);
--- src/redux/saga.js
+++ src/redux/saga.js
@@ -2,19 +2,14 @@ import { takeLatest, all, put, fork, call } from 'redux-saga/effects';
 import * as actions from './action';
 import { getFlickr } from './api';
 
-/*
-  takeLatest : 인수로 action type을 받아서 해당 액션타입 발생시 제일 최근에 발생한 함수 호출
-  takeEvery : '' action type이 발생할 때마다
-  call : 특정 함수를 동기적으로 호출
-  fork : 특정 함수를 비동기적으로 호출
-  put : 기존 dispatch동일한 기능
-  all : fork함수를 그룹화해서 실행할떄
-*/
-
+//action 타입에 따라 변경될 generator함수 정의
 export function* onLoadFlickrAsync(action){
-  console.log(action);
+  
+  //정상적으로 작동시
   try {
-    const response = yield call(getFlickr, action.tag);    
+    //getFlickr함수에 action객체로 넘어온 tag값을 인수로 전달해서 호출
+    const response = yield call(getFlickr, action.tag);  
+    //반환된 결과값을 FLICKR_SUCCESS타입 액션의 payload에 담아서 다시 reducer에 전달  
     yield put({type: actions.FLICKR_SUCCESS, payload: response.data.photos.photo})   
   }
   catch (error) {
@@ -22,10 +17,12 @@ export function* onLoadFlickrAsync(action){
   }
 }
 
+//FLICKR_START타입의 액션 발생시 위의 onLoadFlickrAsync함수 실행
 export function* onLoadFlickr(){
   yield takeLatest(actions.FLICKR_START, onLoadFlickrAsync );
 }
 
+//위의 함수를 비동기적으로 호출
 export default function* rootSaga(){
   yield all([
     fork(onLoadFlickr)
