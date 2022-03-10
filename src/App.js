7c0fb126aeef8da97433494f3758d2dd1372e906
tubi55 <tubi55@nate.com>
2022-03-10 21:28:12 +0900

    inputBox

--- src/App.js
+++ src/App.js
@@ -3,28 +3,45 @@ import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import *  as actions from './redux/action';
 
-export default function App() {
-  //검색어를 담을 state생성
-  const [tag, setTag] = useState('ocean');
-  //구조분해 할당으로 store.flickrReducer안의 객체에서 flickr값만 뽑음
+export default function App() {  
+  const [search, setSearch] = useState('');
+  const [tag, setTag] = useState('ocean'); 
   const {flickr} = useSelector( store => store.flickrReducer );
-  const dispatch = useDispatch();
-  console.log(flickr);
+  const dispatch = useDispatch();  
 
-  //컴포넌트 생성시 FLICKR_START액션타입에 tag를 담아서 reducer에 전달
+  const updateSearch = () => {
+    setTag(search);
+    setSearch('');
+  }
+
+  
   useEffect(()=>{
     dispatch({type: actions.FLICKR_START, tag })
-  },[]);
+  },[tag]);
 
   return (
     <div className="App">
       <h1>Flickr Saga</h1>
-      {flickr.map((item,idx) => (
-        <article key={idx}>
-          <h2>{item.title}</h2>
-          <img src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`} />
-        </article>
-      ))}
+
+      <section id="searchBox">
+        <input 
+          type="text" 
+          value={search}
+          onChange={ e => setSearch(e.target.value)}
+        />
+
+        <button onClick={updateSearch}>Search</button>
+      </section>
+
+      <section id="showBox">
+        {flickr.map((item,idx) => (
+          <article key={idx}>
+            <h2>{item.title}</h2>
+            <img src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`} />
+          </article>
+        ))}
+      </section>
+      
     </div>
   );
 }
